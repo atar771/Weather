8a5cd24e149565596c0c703a9ad99081325ceb64
atar771 <Atar771@gmail.com>
2025-01-04 21:20:51 +0200

    streamlit demo 1

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-Location = input("Where do you want to check the weather?")
-print(f"Weather in {Location} is 72 degrees with clear skies.")
+import streamlit as st
+
+st.markdown("""
+
+# Title    
+## Subtitle
+
+- bullet 1
+- bullet 2
+- bullet 3
+
+>Amazing Quote
+
+""")
