7929a21aedb05438dcaaa087bf2d03bfb619b379
atar771 <Atar771@gmail.com>
2025-01-05 11:21:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,51 @@
+from http.client import responses
+
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 
+##Weather_location_ask=input ("Search City")
+##print ()
+##import requests
+##url =f"https://api.openweathermap.org/data/3.0/onecall?lat"
+##print(url)
+##response = requests.get("https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
 
-secret = st.secrets["my_secret"]
-st.text(f"my secret is {secret}, dont tell anyone")
+##secret = st.secrets["my_secret"]
+##st.text(f"my secret is {secret}, dont tell anyone")
 
 st.markdown("""
-# Title    
-## Subtitle
+# Weather    
+## input ("Search City")
 
-- bullet 1
-- bullet 2
-- bullet 3
+##- bullet 1
+##- bullet 2
+##- bullet 3
 
->Amazing Quote
-""")
+##>Amazing Quote
+##""")
 
 # Create a radio button widget for selecting the plot type
-plot_type = st.radio("Which plot do you want to see?", ["Seaborn", "Plotly"])
+##plot_type = st.radio("Which plot do you want to see?", ["Seaborn", "Plotly"])
 
 # Load the dataset
-df = sns.load_dataset("penguins")
+##df = sns.load_dataset("penguins")
 
-if plot_type == "Seaborn":
+##if plot_type == "Seaborn":
     # Create a Seaborn scatter plot
-    fig, ax = plt.subplots()
-    sns.scatterplot(data=df, x="flipper_length_mm", y="bill_length_mm", hue="species", ax=ax)
-    st.pyplot(fig)
-elif plot_type == "Plotly":
-    # Create a Plotly scatter plot
-    fig = px.scatter(
-        df,
-        x="flipper_length_mm",
-        y="bill_length_mm",
-        color="species",
-        title="Penguins: Flipper Length vs Bill Length"
-    )
-    st.plotly_chart(fig)
+##    fig, ax = plt.subplots()
+##    sns.scatterplot(data=df, x="flipper_length_mm", y="bill_length_mm", hue="species", ax=ax)
+##    st.pyplot(fig)
+##elif plot_type == "Plotly":
+##    # Create a Plotly scatter plot
+##    fig = px.scatter(
+##        df,
+##        x="flipper_length_mm",
+##        y="bill_length_mm",
+##        color="species",
+##        title="Penguins: Flipper Length vs Bill Length"
+##    )
+##    st.plotly_chart(fig)
 
 
-##url ="https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
